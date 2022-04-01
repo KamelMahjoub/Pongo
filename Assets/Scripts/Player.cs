f60bdb03d7ae42c96c4e67d5a8c48b10c47658cd
KamelMahjoub <kamel.mahjoub@outlook.com>
2022-04-01 16:30:33 +0100

    Initialized the player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private bool isPlayer1;
+    public Rigidbody2D playerRb;
+
+
+ private void Start()
+ {
+    playerRb = GetComponent<Rigidbody2D>();
+ }
+
+ private void Update()
+ {
+     
+ }
+ 
+ 
+ 
+ 
+ 
+}
