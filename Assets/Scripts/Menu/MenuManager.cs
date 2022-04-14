91d5a6aae08348e3f834cb003d31399bc1fc9f40
KamelMahjoub <kamel.mahjoub@outlook.com>
2022-04-14 14:25:28 +0100

    Files cleanup

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -12,10 +12,8 @@ namespace SlimUI.ModernMenu
     public class MenuManager : MonoBehaviour
     {
         Animator cameraObject;
-        public enum Theme {custom1, custom2, custom3};
+        
         [Header("THEME SETTINGS")]
-        public Theme theme;
-        int themeIndex;
         public FlexibleUIData themeController;
         
         [Header("SFX")] 
@@ -41,13 +39,7 @@ namespace SlimUI.ModernMenu
         private GameObject settingsCanvas;
         [SerializeField]
         private GameObject matchCanvas;
-       
-        /*
-        [Header("LOADING SCREEN")]
-        public GameObject loadingMenu;
-        public Slider loadBar;
-        public TMP_Text finishedLoadingText;
-*/
+        
 
         private void Awake()
         {
@@ -71,20 +63,10 @@ namespace SlimUI.ModernMenu
         }
         
         //Sets the colours of the menu
-        private void SetThemeColors(){
-            if(theme == Theme.custom1){
-                themeController.currentColor = themeController.custom1.graphic1;
+        private void SetThemeColors()
+        {
+            themeController.currentColor = themeController.custom1.graphic1;
                 themeController.textColor = themeController.custom1.text1;
-                themeIndex = 0;
-            }else if(theme == Theme.custom2){
-                themeController.currentColor = themeController.custom2.graphic2;
-                themeController.textColor = themeController.custom2.text2;
-                themeIndex = 1;
-            }else if(theme == Theme.custom3){
-                themeController.currentColor = themeController.custom3.graphic3;
-                themeController.textColor = themeController.custom3.text3;
-                themeIndex = 2;
-            }
         }
         
         //Opens the match menu
