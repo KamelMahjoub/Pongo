3a8454a1d6cd6ffd2be6e30d58e62a38fed8ea46
KamelMahjoub <kamel.mahjoub@outlook.com>
2022-04-26 13:47:08 +0100

    Added a powerup manager script

--- Assets/Scripts/Manager/PowerupManager.cs
+++ Assets/Scripts/Manager/PowerupManager.cs
@@ -8,32 +8,69 @@ using Random = UnityEngine.Random;
 
 public class PowerupManager : MonoBehaviour
 {
-  
-  [SerializeField]
-  private GameObject[] powerupArray;
+    [Header("POWERUPS LIST")]
+    [SerializeField] 
+    private GameObject[] powerupArray;
 
-  private int nbPowerups;
+    private int nbPowerups;
 
-
-  private void Awake()
-  {
-    Init();
-  }
+    public bool IsPowerupOnField {get; set; }
 
 
-  private void Init()
-  {
-    nbPowerups = powerupArray.Length;
-  }
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        if (!IsPowerupOnField)
+        {
+            StartCoroutine(SpawnPowerUpRoutine());
+        }
+    }
+
+    private void Init()
+    {
+        nbPowerups = powerupArray.Length;
+        IsPowerupOnField = false;
+    }
 
 
-  private int GetRandomPowerupIndex()
-  {
-    return Random.Range(0, nbPowerups);
-  }
-  
-  
-  
-  
-  
+    private int GetRandomPowerupIndex()
+    {
+        return Random.Range(0, nbPowerups);
+    }
+
+    private float GetRandomXPosition()
+    {
+        float xMinPosition = 0.5f;
+        float xMaxPosition = 5;
+        float xPosition;
+
+        do
+        {
+            xPosition = Random.Range(-xMaxPosition, xMaxPosition);
+        } while (xPosition > -xMinPosition && xPosition < xMinPosition);
+
+        return xPosition;
+    }
+    
+    private Vector2 GetRandomPosition()
+    {
+        float yPosition = 3;
+        return new Vector2(GetRandomXPosition(), Random.Range(-yPosition, yPosition));
+    }
+
+
+    private IEnumerator SpawnPowerUpRoutine()
+    {
+        int spawnInterval = 5;
+        yield return new WaitForSeconds(spawnInterval);
+
+        Instantiate(powerupArray[GetRandomPowerupIndex()], GetRandomPosition(),
+            powerupArray[GetRandomPowerupIndex()].transform.rotation);
+
+        IsPowerupOnField = true;
+    }
 }
--- Assets/Scripts/Match/Powerups/Powerup.cs
+++ Assets/Scripts/Match/Powerups/Powerup.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 public abstract class Powerup : MonoBehaviour
 {
+    private PowerupManager powerupScript;
+    
     protected abstract void ActivateEffect();
     
     private void OnTriggerEnter2D(Collider2D col)
     {
         Destroy(gameObject);
         ActivateEffect();;
+        powerupScript.IsPowerupOnField = false;
     }
 }
